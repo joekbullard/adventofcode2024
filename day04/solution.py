daa7cfd645aed7d39ae53db6ef5068827ef576d1
Joe Bullard <joekbullard@gmail.com>
2024-12-06 17:43:17 +0000

    day4 solutions

--- day04/solution.py
+++ day04/solution.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 class Solution:
 
     def __init__(self, input_path: str):
@@ -12,6 +13,29 @@ class Solution:
 
         return lines
 
+    def _search(self, sequence, direction, origin_x, origin_y, index=0) -> int:
+
+        if index >= len(sequence):  # Entire sequence matched
+            print("xmas found")
+            return True
+
+        x_diff, y_diff = direction
+        loc_x = origin_x + x_diff
+        loc_y = origin_y + y_diff
+
+        if (
+            loc_x < 0
+            or loc_y < 0
+            or loc_y >= len(self.lines)
+            or loc_x >= len(self.lines[loc_y])
+        ):
+            return False
+
+        if self.lines[loc_y][loc_x] == sequence[index]:
+            return self._search(sequence, direction, loc_x, loc_y, index + 1)
+
+        return False
+
     def part1(self) -> int | str:
         """Solves part 1 of the challenge using the provided input.
 
@@ -22,7 +46,27 @@ class Solution:
             int | str: The answer to part 1.
         """
 
-        return 0
+        directions = (
+            (0, -1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 1),
+            (-1, 0),
+            (-1, -1),
+        )
+        total = 0
+        for count_y, row in enumerate(self.lines):
+            for col in range(len(row)):
+                current_char = self.lines[count_y][col]
+                if current_char == "X":
+                    for dir in directions:
+                        count = self._search("MAS", dir, col, count_y)
+                        if count:
+                            total += 1
+
+        return total
 
     def part2(self) -> int | str:
         """Solves part 2 of the challenge using the provided input.
@@ -33,9 +77,24 @@ class Solution:
         Returns:
             int | str: The answer to part 2.
         """
+        MASSAM = ("MAS", "SAM")
 
-        return 0
-    
+        count = 0
+        for y, line in enumerate(self.lines[1:-1], 1):
+            for x, char in enumerate(line[1:-1], 1):
+                if char == "A":
+                    top_right = self.lines[y - 1][x + 1]
+                    bottom_left = self.lines[y + 1][x - 1]
+                    bottom_right = self.lines[y + 1][x + 1]
+                    top_left = self.lines[y - 1][x - 1]
+                    if (
+                        top_right + char + bottom_left in MASSAM
+                        and top_left + char + bottom_right in MASSAM
+                    ):
+                        count += 1
+
+        return count
+
 
 input_path = Path(__file__).parent / "input.txt"
 solution = Solution(input_path)
