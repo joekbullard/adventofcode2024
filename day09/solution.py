80b2df1372cc219bad7c956bdb3f957ee2b816ff
Joe Bullard <joekbullard@gmail.com>
2024-12-12 09:28:21 +0000

    day 9 pt1 solution

--- day09/solution.py
+++ day09/solution.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
+
 class Solution:
 
     def __init__(self, input_path: str):
         self.input_path = input_path
-        self.lines = self._process_input()
+        self.line = self._process_input()
 
     def _process_input(self):
         with open(self.input_path) as input:
-            lines = input.read().splitlines()
+            line = input.readline()
 
-        return lines
+        return line
 
     def part1(self) -> int | str:
         """Solves part 1 of the challenge using the provided input.
@@ -22,7 +23,33 @@ class Solution:
             int | str: The answer to part 1.
         """
 
-        return 0
+        index_list = [
+            "." if i % 2 == 1 else int(i / 2)
+            for i in range(len(self.line))
+            for _ in range(int(self.line[i]))
+        ]
+
+        n = len(index_list)
+
+        j = 0
+        i = 0
+        total = 0
+
+        while i < (n - j):
+            if index_list[i] == ".":
+                while i < (n - 1 - j):
+                    if isinstance(index_list[n - 1 - j], int):
+                        index_list[i], index_list[n - 1 - j] = (
+                            index_list[n - 1 - j],
+                            index_list[i],
+                        )
+                        j += 1
+                        break
+                    j += 1
+            total += i * index_list[i]
+            i += 1
+
+        return total
 
     def part2(self) -> int | str:
         """Solves part 2 of the challenge using the provided input.
@@ -35,9 +62,9 @@ class Solution:
         """
 
         return 0
-    
 
-input_path = Path(__file__).parent / "input.txt"
+
+input_path = Path(__file__).parent / "example.txt"
 solution = Solution(input_path)
 
 print(f"Solution to part 1: {solution.part1()}")
